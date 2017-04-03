3d7a3b5c7f6141ffb5fe270a2a3fa34db37aa125
Muhammad Ali <mycodeonion@gmail.com>
2017-04-03 09:06:28 -0700

    Added more details
    Added more details

--- Matlab-Tutorials/tut3_faceDetection.m
+++ Matlab-Tutorials/tut3_faceDetection.m
@@ -3,10 +3,16 @@ clear all;
 rawFaces = imread('assets/faces.jpg');
 % imshow(rawFaces);
 
-detector = vision.CascadeObjectDetector('Mouth');
 
-faceBoundingBox = step(detector, rawFaces);
+% Following will create a "detector object" which detects objects using the Viola-Jones algorithm
+% By default, it detects Faces
+% CascadeObjectDetector can receive parameters like CascadeObjectDetector()
+detector = vision.CascadeObjectDetector;
 
-facesDetected = insertObjectAnnotation(rawFaces, 'rectangle', faceBoundingBox, 'Target');
+% Detects the face
+faceBoundingBox = step(detector, rawFaces); 
+
+
+facesDetected = insertObjectAnnotation(rawFaces, 'rectangle', faceBoundingBox, 'Face');
 
 imshow(facesDetected);

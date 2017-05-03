83af3defddb0447c7750e795eac996de73576d18
Muhammad Ali <mycodeonion@gmail.com>
2017-05-03 22:34:05 +0500

    Tutorial 3 is complete

---
+++ Matlab-Tutorials/Tutorial_03_Variables_Arithmetic_operations_Functions_in_Matlab.m
@@ -0,0 +1,29 @@
+% Basic arithmetic operations
+x = 100;
+a = x + 100;
+b = x - 110;
+c = x * 400;
+d = x / 50;
+
+% Change value of previously defined variable like:
+x = 5;
+
+% Powers
+y = x^5;
+
+% SquareRoot
+e = sqrt(x);
+f = sqrt(16);
+
+% Logarithmic Operations
+g = log(105);
+h = log10(105);
+i = log10(pi);
+j = log(-1);
+
+% Trigonometric Ratios
+boss = pi;
+k = sin(4*boss/3);
+l = tan(boss*boss);
+
+
